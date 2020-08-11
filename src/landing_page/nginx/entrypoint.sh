3c15beb95e3b991b773693a8b186c84715c9f58f
Michael Bao <mike@grchive.com>
2020-08-11 15:48:07 -0400

    opengraph and twitter. better integrations with nginx

--- src/landing_page/nginx/entrypoint.sh
+++ src/landing_page/nginx/entrypoint.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 set -xe
 
-certbot -n --nginx --agree-tos --email mike@grchive.com -d grchive.com -d www.grchive.com --redirect
-nginx -s stop
+if [[ ! -v DISABLE_CERTBOT ]]; then
+    certbot -n --nginx --agree-tos --email mike@grchive.com -d grchive.com -d www.grchive.com --redirect
+    nginx -s stop
+fi
+
 exec /docker-entrypoint.sh nginx -g "daemon off;"
